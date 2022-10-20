c95fe6154c8ed3191cbf386f6e108be8c4887074
Stephen Doty <stephendoty826@yahoo.com>
2022-10-20 14:41:11 -0500

    installed react-bootstrap

--- blackjack-with-react/src/App.js
+++ blackjack-with-react/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <>
       Nothing here
-      <img src='./images/1_of_clubs.png' style={{height: '500px'}} alt=''/>
+      {/* <img src='./images/1_of_clubs.png' style={{height: '500px'}} alt=''/> */}
+      <Button variant="primary">Primary</Button>{' '}
     </>
   );
 }
