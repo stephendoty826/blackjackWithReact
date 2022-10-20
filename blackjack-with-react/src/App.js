899ecabd5875daf601f57b30ab5f83de3f7e47a7
Stephen Doty <stephendoty826@yahoo.com>
2022-10-20 11:01:21 -0500

    committing

--- blackjack-with-react/src/App.js
+++ blackjack-with-react/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <>
-      Nothing here 
+      Nothing here
+      <img src='./images/1_of_clubs.png' style={{height: '500px'}} alt=''/>
     </>
   );
 }
